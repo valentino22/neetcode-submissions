9efbd1df1bf95b290231224f006299742460e989
Szabolcs Ferenczi <szabi22@gmail.com>
2026-08-26 18:04:46 +0200

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) { return false;}
+
+        let count = new Map();
+
+        for (const char of t){
+            count.set(char, (count.get(char) ?? 0) + 1);
+        }
+        
+        for (const char of s) {
+            if (!count.has(char)) {return false}
+            count.set(char, count.get(char) - 1);
+
+            if (count.get(char) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
